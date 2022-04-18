438ebb5a013880a2abe56d573613e6cbc101bc43
Jashua R. White <jashuawhite@gmail.com>
2022-04-18 15:43:52 -0400

    add codepen embed

--- src/app/shared/embed-code-pen.service.ts
+++ src/app/shared/embed-code-pen.service.ts
@@ -5,23 +5,22 @@ import { Injectable } from '@angular/core';
 })
 export class EmbedCodePenService {
 
-  private CODEPEN_REGEX: RegExp = new RegExp(`https://codepen.io/jashuawhite/pen/([a-zA-Z0-9]+)`)
+  private CODEPEN_REGEX: RegExp = new RegExp(`https://codepen.io/jashuawhite/pen/([a-zA-Z0-9]+)`);
   private CODEPEN_SCRIPT_ID: string = 'codepen-script';
   private CODEPEN_SCRIPT_SRC: string = 'https://cpwebassets.codepen.io/assets/embed/ei.js';
 
   constructor() { }
 
   public init() {
-      this.loadCodePens()
+      this.loadCodePens();
   }
 
   private loadCodePens() {
-      const script = document.createElement('script')
-      script.id = this.CODEPEN_SCRIPT_ID
-      script.src = this.CODEPEN_SCRIPT_SRC
-      script.type = 'text/javascript'
-      script.defer = true
-      document.querySelector('head')?.appendChild(script)
-    
+      const script = document.createElement('script');
+      script.id = this.CODEPEN_SCRIPT_ID;
+      script.src = this.CODEPEN_SCRIPT_SRC;
+      script.type = 'text/javascript';
+      script.defer = true;
+      document.querySelector('head')?.appendChild(script);
   }
 }
